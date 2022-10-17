1c01c3543a5c5f9f833403973a3f47524f0f9998
NguyenThiThanhHien2054050064 <2054050064hien@ou.edu.vn>
2022-10-17 10:37:00 +0700

    Xu ly cong

--- calculat/Form1.Designer.cs
+++ calculat/Form1.Designer.cs
@@ -103,6 +103,7 @@ namespace calculat
             this.button1.TabIndex = 4;
             this.button1.Text = "cộng";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
